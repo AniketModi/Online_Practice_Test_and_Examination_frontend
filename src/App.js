d7818f3efaf505f1299cb12b60b3e4ec62466c7b
Jay30801 <201801133@daiict.ac.in>
2021-06-15 15:43:46 +0530

    login done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './login';
 import Signup from './Signup';
+import Main from './Main';
 import React from 'react';
 import {BrowserRouter , Route , Link , NavLink , Switch} from 'react-router-dom';
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Switch>
             <Route path="/" component={Login} exact={true}/>
             <Route path="/UP" component={Signup} exact={true}/>
+            <Route path="/main" component={Main} exact={true}/>
         </Switch>    
       </BrowserRouter>
     </div>
