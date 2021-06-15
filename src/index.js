6aea6e88d0bbef89c662f5a306f947a01793888b
Jay30801 <76864215+Jay30801@users.noreply.github.com>
2021-06-15 15:46:17 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Profile_Page />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
